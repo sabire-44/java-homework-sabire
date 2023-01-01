6a7f694e7d071a8e37f7b3a6631515ce702bec19
Sabire Alkin <sabirealkin@gmail.com>
2022-12-31 23:27:18 -0400

    day 01 2nd part of hw

---
+++ src/main/java/org/week1/day2/PrimitiveNumbersPart2HW.java
@@ -0,0 +1,77 @@
+package org.week1.day2;
+
+public class PrimitiveNumbersPart2HW {
+  public static void main(String []args) {
+
+      byte maxRange = 127;
+      System.out.println("Max Range:"+ maxRange );
+      byte minRange = -128;
+      System.out.println("Min Range:"+ minRange );
+      byte anyRange = 101;
+      System.out.println("Any Range:"+ anyRange );
+      byte randomRange = 1;
+      System.out.println("Random Range:"+ randomRange );
+
+      short maxShortRange = 32767;
+      System.out.println("Max Short Range:"+ maxShortRange );
+      short minShortRange = -32768;
+      System.out.println("Min Short Range:"+ minShortRange );
+      short anyShortRange = 20000;
+      System.out.println("Any Short Range:"+ anyShortRange );
+      short randomShortRange = -12758;
+      System.out.println("Random Short Range:"+ randomShortRange );
+
+      int maxIntRange = 2_147_483_647;
+      System.out.println("Max Int Range:"+ maxIntRange );
+      int minIntRange = -2_147_483_648;
+      System.out.println("Min Int Range:"+ minIntRange );
+      int anyIntRange = 1234566;
+      System.out.println("Any Int Range:"+ anyIntRange );
+      int randomIntRange = -3_456_798;
+      System.out.println("Random Int Range:"+ randomIntRange );
+
+      long maxLongRange = 9_372_036_854_775_808L;
+      System.out.println("Max Long Range:"+ maxLongRange );
+      long minLongRange = -9_223_372_036_854_775_807L;
+      System.out.println("Min Long Range:"+ minLongRange );
+      long anyLongRange = 4_123_456_678_922L;
+      System.out.println("Any Long Range:"+ anyLongRange );
+      long randomLongRange = -3_456_798_999_404L;
+      System.out.println("Random Long Range:"+ randomLongRange );
+
+      boolean myBooleanValue = true;
+      System.out.println("My boolean Value:"+ myBooleanValue );
+      boolean iAmGoingToBe = true;
+      System.out.println("I Am Going To Be:"+ iAmGoingToBe );
+
+
+  }
+}
+// public static void main(String[]args) {
+
+
+/** es:
+        1-) Create a class november23.hw1.part2.PrimitiveNumbersPart2HW main method and print the values:
+byte:
+create a byte variable name called maxRange and assign the value of 127
+print the variable called maxRange. create a byte variable name called minRange and assign the value of -128
+print the variable called minRange. create a byte variable name called anyRange and assign the value of 101
+print the variable called anyRange. create a byte variable name called randomRange and assign the value of 1
+print the variable called randomRange. short: create a short variable name called maxShortRange and assign the value of 32767
+print the variable called maxShortRange. create a short variable name called minshortRange and assign the value of -32768
+print the variable called minRange. create a short variable name called anyshortRange and assign the value of 20000
+print the variable called anyRange. create a short variable name called randomShortRange and assign the value of -12758
+print the variable called randomRange. int: create a int variable name called maxInttRange and assign the value of 2_147_483_647
+print the variable called maxIntRange. create a int variable name called minIntRange and assign the value of -2_147_483_648
+print the variable called minIntRange. create a int variable name called anyIntRange and assign the value of 1234566
+print the variable called anyIntRange. create a int variable name called randomIntRange and assign the value of -3_456_798
+print the variable called randomIntRange. long: (don't forget to put the capital L at the end of each number)
+create a long variable name called maxLongtRange and assign the value of 9_223_372_036_854_775_808
+print the variable called maxLongtRange. create a long variable name called minLongRange and assign the value of -9_223_372_036_854_775_807
+print the variable called minLongRange. create a long variable name called anyLongRange and assign the value of 4_123_456_678_922
+print the variable called anyLongRange. create a long variable name called randomLongRange and assign the value of -3_456_798_999_404
+print the variable called randomLongRange. boolean :
+create a boolean variable name called myBooleanValue and assign the value of true
+print the variable called myBooleanValue. create a boolean variable name called iAmGoingToBe and assign the value of true
+print the variable called myBooleanValue.
+ */
